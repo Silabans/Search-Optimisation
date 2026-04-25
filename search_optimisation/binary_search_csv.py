47b94ffd66a153aee52ab446a4dadd919c6132c9
Matthew Zimra Yedija Silaban <matthew.yedija@gmail.com>
2026-04-25 18:53:41 +0800

    add code for termination if target is not found, finish binary search logic, and account for the bit size of the headers

--- search_optimisation/binary_search_csv.py
+++ search_optimisation/binary_search_csv.py
@@ -7,9 +7,11 @@ def binary_search_csv(filename, target, column_i=0):
 
     with open(filename, "rb") as f:
         #This reads the file as raw binary, which allows us to get the file size
-        #the cursor around in the file (i.e to move to the middle)
+        #and move the cursor around in the file (i.e to move to the middle)
+        header = f.readline()
+        header_size = f.tell()
         
-        low = 0
+        low = header_size
         high = file_size
 
         while low < high:
@@ -20,11 +22,17 @@ def binary_search_csv(filename, target, column_i=0):
                 f.readline()
 
             current_position = f.tell()
+
+            # If readline() takes us to the end of the file, move the high pointer back
+            # If mid == header_size, that means that both low and high have the value of header_size
+            if current_position >= high and mid != header_size:
+                high = mid
+                continue
             
             line = f.readline().decode("utf-8").strip()
             if not line:
                 high = mid
-                continue
+                break
             
             reader = csv.reader([line])
             row = next(reader)
